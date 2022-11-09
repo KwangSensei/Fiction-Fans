602c8d8033e61f5d4bb8023581f64b581c7f06ca
Chinapat <chinapat.r@ku.th>
2022-11-09 16:59:49 +0700

    add docstring in fiction_view

--- fiction_fans/views/fiction_view.py
+++ fiction_fans/views/fiction_view.py
@@ -1,6 +1,6 @@
 """Create view for reading fiction's story."""
 from django.shortcuts import render
-from ..models.fiction_model import FictionTitle, FictionChapter
+from ..models import FictionTitle, FictionChapter
 from django.views import generic
 
 # Create your views here.
@@ -11,17 +11,24 @@ class ChapterView(generic.DetailView):
     template_name = "fiction_fans/chapter_page.html"
     model = FictionChapter
 
-    def get_queryset(self):
-        fiction_pk = self.kwargs.get("fiction_pk")
-        return FictionTitle.objects.filter(pk=fiction_pk)
-
     def get_object(self, queryset=None):
-        queryset = self.get_queryset()
+        """ 
+        Get chapter object from query and return it to render method.
+        """
         chapter_pk = self.kwargs.get("chapter_pk")
         queryset = FictionChapter.objects.filter(pk=chapter_pk)
         return queryset.get()
 
 class FictionView(generic.DetailView):
+    """Create view for viewing detail of fiction
+    
+        this page will display:
+        1. Synopsis
+        2. Fiction title
+        3. list of chapters
+        4. fiction genre
+        5. fiction cover
+    """
     template_name = "fiction_fans/fiction_page.html"
     model = FictionTitle
 
